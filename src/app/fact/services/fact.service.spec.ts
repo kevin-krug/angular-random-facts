b01a50e4c3bddea63aeb26b3eecdd639cf1765a8
Kevin.Krug <kevin.d.krug@googlemail.com>
2025-05-08 11:27:56 +0200

    adds fact service

---
+++ src/app/fact/services/fact.service.spec.ts
@@ -0,0 +1,20 @@
+import { TestBed } from '@angular/core/testing';
+
+import { FactService } from './fact.service';
+import { provideHttpClientTesting } from '@angular/common/http/testing';
+import { provideHttpClient } from '@angular/common/http';
+
+describe('FactService', () => {
+  let service: FactService;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      providers: [provideHttpClient(), provideHttpClientTesting()],
+    });
+    service = TestBed.inject(FactService);
+  });
+
+  it('should be created', () => {
+    expect(service).toBeTruthy();
+  });
+});
---
+++ src/app/fact/services/fact.service.ts
@@ -0,0 +1,35 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { catchError, map, Observable, of, startWith } from 'rxjs';
+import { environment } from '../../../environments/environment';
+
+export interface IFactData {
+  id: string;
+  text: string;
+  source: string;
+  source_url: string;
+  language: string;
+  permalink: string;
+}
+
+export type TFactState =
+  | { state: 'loading' }
+  | { state: 'error'; error: any }
+  | { state: 'loaded'; data: IFactData };
+
+
+@Injectable({
+  providedIn: 'root'
+})
+export class FactService {
+
+  constructor(private http: HttpClient) { }
+
+  fetchFact(): Observable<TFactState> {
+    return this.http.get<IFactData>(environment.apiUrlEndpoint).pipe(
+      map(data => ({ state: 'loaded', data }  as const)),
+      catchError(error => of({ state: 'error', error }  as const)),
+      startWith({ state: 'loading' }  as const)
+    );
+}
+}
