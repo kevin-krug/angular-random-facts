a2b57cf897f2a4afc52aeeeca2b7f5bbfbb4c985
Kevin.Krug <kevin.d.krug@googlemail.com>
2025-05-08 11:31:40 +0200

    :sparkles: fetch and display random fact

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { FactService, TFactState } from './fact/services/fact.service';
+import { Observable  } from 'rxjs';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, CommonModule ],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'random-facts-app';
+  data$!: Observable<TFactState>;
+
+  constructor(
+		private factService: FactService,
+	) {}
+
+  // TODO: move to fact component
+  ngOnInit() {
+    this.data$ = this.factService.fetchFact();    
+  }
 }
