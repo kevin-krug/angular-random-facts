be1ab17bd924d63cf80e5c5bceffaa789d94d369
Kevin.Krug <kevin.d.krug@googlemail.com>
2025-05-08 16:56:27 +0200

    adds search favorites component

---
+++ src/app/search/search.component.ts
@@ -0,0 +1,26 @@
+import { Component } from '@angular/core';
+import { Observable } from 'rxjs';
+import { IFactData } from '../fact/services/fact.service';
+import { FavoritesService } from '../favorites/services/favorites.service';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-search',
+  imports: [CommonModule],
+  templateUrl: './search.component.html',
+  styleUrl: './search.component.css'
+})
+export class SearchComponent {
+  favorites$: Observable<IFactData[]>;;
+  
+  constructor(
+    private favoritesService: FavoritesService
+  ) {
+    this.favorites$ = this.favoritesService.favorites$;
+  }
+
+  // TODO: add correct event type
+  onSelect(event: any) {
+    this.favoritesService.setSelectedFact(event?.target?.value)
+  }
+}
